17e713891439adbb2909602624bdc4a516d1b91b
shendong <seanshen@zju.edu.cn>
2019-06-14 20:36:41 +0800

    update

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-sh -s $(which zsh)
+chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
